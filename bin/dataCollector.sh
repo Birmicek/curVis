85a5e7e0cdffd20e8cb36fb83176b7fbff6081dc
Peter Kolarik <kolarik.peter93@gmail.com>
2020-03-25 06:17:43 -0400

    Added feature: remove data.zip file before new pull

--- bin/dataCollector.sh
+++ bin/dataCollector.sh
@@ -6,8 +6,14 @@ LOG_DIR=$BASE/logs
 OUT_LOG=$LOG_DIR/output.log
 DATA_FILE=$BASE/data.zip
 
-#make sure the BASE directory exists and if not, create it
+#make sure the BASE & LOG_DIR directory exists and if not, create it
 mkdir -p $LOG_DIR
+
+#delete data.zip file if exists
+if [ -f $DATA_FILE ]
+then
+	rm -f $DATA_FILE
+fi
 
 #put timestamp into output.log
 echo "==============================================" >> $OUT_LOG
@@ -15,4 +21,4 @@ echo `date`: >> $OUT_LOG
 echo >> $OUT_LOG
 
 #download the zip file from ECB webpage
-curl -L "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip?ee318a8fef4b64837f6f7f900275b4c7" > $BASE/data.zip 2>> $OUT_LOG
+curl -L "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip?ee318a8fef4b64837f6f7f900275b4c7" > $DATA_FILE 2>> $OUT_LOG
