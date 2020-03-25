8d2f4c7d77c6eef451030c6e6300cb3614c57f27
Peter Kolarik <kolarik.peter93@gmail.com>
2020-03-25 06:59:27 -0400

    Added feature: /data dir; modified IF condition; remove files in data/ dir; unzip file and rename it

--- bin/dataCollector.sh
+++ bin/dataCollector.sh
@@ -3,16 +3,20 @@
 #related directories and files
 BASE=$HOME/app/curVis
 LOG_DIR=$BASE/logs
+DATA_DIR=$BASE/data
 OUT_LOG=$LOG_DIR/output.log
-DATA_FILE=$BASE/data.zip
+DATA_FILE=$DATA_DIR/data.zip
+UNZIPPED_DATA=$DATA_DIR/currency_data.csv
+UNZIP=`which unzip`
+CURL=`which curl`
 
-#make sure the BASE & LOG_DIR directory exists and if not, create it
-mkdir -p $LOG_DIR
+#make sure the BASE, LOG_DIR & DATA_DIR directories exist and if not, create them
+mkdir -p $LOG_DIR $DATA_DIR
 
-#delete data.zip file if exists
-if [ -f $DATA_FILE ]
+#delete data.zip and currency_data.csv files if exist
+if [ -f $DATA_FILE ] || [ -f $UNZIPPED_DATA ]
 then
-	rm -f $DATA_FILE
+	rm -f $DATA_FILE $UNZIPPED_DATA
 fi
 
 #put timestamp into output.log
@@ -21,4 +25,7 @@ echo `date`: >> $OUT_LOG
 echo >> $OUT_LOG
 
 #download the zip file from ECB webpage
-curl -L "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip?ee318a8fef4b64837f6f7f900275b4c7" > $DATA_FILE 2>> $OUT_LOG
+$CURL -L "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip?ee318a8fef4b64837f6f7f900275b4c7" > $DATA_FILE 2>> $OUT_LOG
+
+#Unzip data.zip and rename the file
+$UNZIP $DATA_FILE -d $DATA_DIR > /dev/null 2>&1 && mv $DATA_DIR/`echo $(unzip -lqq $DATA_FILE | awk '{print $NF}')` $UNZIPPED_DATA
